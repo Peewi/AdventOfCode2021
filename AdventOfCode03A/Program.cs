444087af1dd513c0ab476301fa7a48311cb82251
Peewi <laursen.lau@gmail.com>
2021-12-03 10:24:50 +0100

    Day 3

---
+++ AdventOfCode03A/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code day 03 part 1");
+var lines = File.ReadAllLines("Input.txt");
+int[] ones = new int[lines[0].Length];
+for (int i = 0; i < lines.Length; i++)
+{
+	for (int j = 0; j < lines[i].Length; j++)
+	{
+		if (lines[i][j] == '1')
+		{
+			ones[j]++;
+		}
+	}
+}
+int gammaRate = 0;
+int epsilonRate = 0;
+for (int i = 0; i < ones.Length; i++)
+{
+	if (ones[^(1+i)] > lines.Length / 2)
+	{
+		gammaRate += (int)Math.Pow(2, i);
+	}
+	else
+	{
+		epsilonRate += (int)Math.Pow(2, i);
+	}
+}
+Console.WriteLine($"Gamme rate: {gammaRate}");
+Console.WriteLine($"Epsilon rate: {epsilonRate}");
+Console.WriteLine($"Gamme * Epsilon: {gammaRate * epsilonRate}");
---
+++ AdventOfCode03B/Program.cs
@@ -0,0 +1,109 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code day 03 part 2");
+var lines = File.ReadAllLines("Input.txt");
+// initial values
+bool[] oxygenValid = new bool[lines.Length];
+int oxygenValidCount = lines.Length;
+bool[] CO2Valid = new bool[lines.Length];
+int CO2ValidCount = lines.Length;
+for (int i = 0; i < lines.Length; i++)
+{
+	oxygenValid[i] = true;
+	CO2Valid[i] = true;
+}
+//
+int oxygenRating = 0;
+int co2Rating = 0;
+// oxygen
+for (int space = 0; space < lines[0].Length; space++)
+{
+	int ones = 0;
+	for (int j = 0; j < lines.Length; j++)
+	{
+		if (oxygenValid[j] && lines[j][space] == '1')
+		{
+			ones++;
+		}
+	}
+	char target = '0';
+	if (ones >= oxygenValidCount / 2)
+	{
+		target = '1';
+	}
+	oxygenValidCount = 0;
+	for (int j = 0; j < lines.Length; j++)
+	{
+		oxygenValid[j] = oxygenValid[j] && lines[j][space] == target;
+		if (oxygenValid[j])
+		{
+			oxygenValidCount++;
+		}
+	}
+	if (oxygenValidCount == 1)
+	{
+		break;
+	}
+}
+for (int i = 0; i < lines.Length; i++)
+{
+	if (oxygenValid[i])
+	{
+		oxygenRating = BinaryStringToInt(lines[i]);
+	}
+}
+// co2
+for (int space = 0; space < lines[0].Length; space++)
+{
+	int ones = 0;
+	for (int j = 0; j < lines.Length; j++)
+	{
+		if (CO2Valid[j] && lines[j][space] == '1')
+		{
+			ones++;
+		}
+	}
+	char target = '0';
+	if (ones < CO2ValidCount / 2)
+	{
+		target = '1';
+	}
+	CO2ValidCount = 0;
+	for (int j = 0; j < lines.Length; j++)
+	{
+		CO2Valid[j] = CO2Valid[j] && lines[j][space] == target;
+		if (CO2Valid[j])
+		{
+			CO2ValidCount++;
+		}
+	}
+	if (CO2ValidCount == 1)
+	{
+		break;
+	}
+}
+for (int i = 0; i < lines.Length; i++)
+{
+	if (CO2Valid[i])
+	{
+		co2Rating = BinaryStringToInt(lines[i]);
+	}
+}
+Console.WriteLine($"Oxygen: {oxygenRating}");
+Console.WriteLine($"CO2: {co2Rating}");
+Console.WriteLine($"Oxygen * CO2: {oxygenRating * co2Rating}");
+
+/// <summary>
+/// What it says
+/// </summary>
+static int BinaryStringToInt(string bin)
+{
+	int retval = 0;
+	for (int i = 0; i < bin.Length; i++)
+	{
+		if (bin[^(1 + i)] == '1')
+		{
+			retval += (int)Math.Pow(2, i);
+		}
+	}
+	return retval;
+}

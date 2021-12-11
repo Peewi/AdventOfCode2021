5022710efd512c7c37e53a42cb1eb1916f04406a
Peewi <laursen.lau@gmail.com>
2021-12-11 11:49:05 +0100

    Day 11

---
+++ AdventOfCode11B/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code day 11 part 2");
+string[] input = File.ReadAllLines("Input.txt");
+int[,] grid = new int[input[0].Length, input.Length];
+bool[,] flashThisStep = new bool[input[0].Length, input.Length];
+for (int line = 0; line < input.Length; line++)
+{
+	for (int i = 0; i < input[line].Length; i++)
+	{
+		grid[i, line] = int.Parse(input[line][i].ToString());
+	}
+}
+int flashCount = 0;
+int stepFlashes = 0;
+for (int i = 0; i < 1000; i++)
+{
+	stepFlashes = 0;
+	flashThisStep = new bool[input[0].Length, input.Length];
+	for (int x = 0; x < grid.GetUpperBound(0) + 1; x++)
+	{
+		for (int y = 0; y < grid.GetUpperBound(1) + 1; y++)
+		{
+			grid[x, y]++;
+		}
+	}
+	for (int x = 0; x < grid.GetUpperBound(0) + 1; x++)
+	{
+		for (int y = 0; y < grid.GetUpperBound(1) + 1; y++)
+		{
+			if (!flashThisStep[x, y] && grid[x, y] > 9)
+			{
+				Flash(x, y);
+			}
+		}
+	}
+	for (int x = 0; x < grid.GetUpperBound(0) + 1; x++)
+	{
+		for (int y = 0; y < grid.GetUpperBound(1) + 1; y++)
+		{
+			if (grid[x, y] > 9)
+			{
+				flashCount++;
+				stepFlashes++;
+				grid[x, y] = 0;
+			}
+		}
+	}
+	Console.WriteLine($"Step {i + 1}, {stepFlashes} flashes this step, {flashCount} total flashes");
+	if (stepFlashes == 100)
+	{
+		break;
+	}
+}
+
+void Flash(int x, int y)
+{
+	flashThisStep[x, y] = true;
+	for (int adjX = Math.Max(x - 1, 0); adjX < Math.Min(x + 2, grid.GetUpperBound(0) + 1); adjX++)
+	{
+		for (int adjY = Math.Max(y - 1, 0); adjY < Math.Min(y + 2, grid.GetUpperBound(1) + 1); adjY++)
+		{
+			if (true)
+			{
+				grid[adjX, adjY]++;
+				if (!flashThisStep[adjX, adjY] && grid[adjX, adjY] > 9)
+				{
+					flashThisStep[adjX, adjY] = true;
+					Flash(adjX, adjY);
+				}
+			}
+		}
+	}
+}

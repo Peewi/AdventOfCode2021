8a192e2d6049554814db464fce1a447a618a1187
Peewi <laursen.lau@gmail.com>
2021-12-20 16:06:28 +0100

    Day 20 Also has an incomplete day 19

---
+++ AdventOfCode19A/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code day 19 part 1");
+string[] input = File.ReadAllLines("Input.txt");
+List<List<(int x, int y, int z)>> scanners = new List<List<(int x, int y, int z)>>();
+foreach (var line in input)
+{
+	if (!string.IsNullOrWhiteSpace(line))
+	{
+		if (line[0..3] == "---")
+		{
+			scanners.Add(new List<(int, int, int)>());
+		}
+		else
+		{
+			var split = line.Split(',');
+			scanners[^1].Add((int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2])));
+		}
+	}
+}
+Console.WriteLine($"Read {scanners.Count} scanners from input.");
+HashSet<(int x, int y, int z)> uniqueBeacons = new HashSet<(int x, int y, int z)>();
+foreach (var beacon in scanners[0])
+{
+	uniqueBeacons.Add(beacon);
+}
+for (int i = 1; i < scanners.Count; i++)
+{
+	List<(int x, int y, int z)> topLeftCandidates = new List<(int x, int y, int z)>();
+	topLeftCandidates.Add(scanners[i][0]);
+	for (int j = 1; j < scanners[i].Count; j++)
+	{
+		bool better = scanners[i][j].x < topLeftCandidates[0].x;
+		better = better || (scanners[i][j].x == topLeftCandidates[0].x
+			&& scanners[i][j].y < topLeftCandidates[0].y);
+		better = better || (scanners[i][j].y == topLeftCandidates[0].y
+			&& scanners[i][j].z < topLeftCandidates[0].z);
+		if (better)
+		{
+			topLeftCandidates.Insert(0, scanners[i][j]);
+		}
+	}
+}
---
+++ AdventOfCode20A/Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code day 20 part 1");
+string[] input = File.ReadAllLines("Input.txt");
+bool[] algo = new bool[input[0].Length];
+for (int i = 0; i < input[0].Length; i++)
+{
+	algo[i] = input[0][i] == '#';
+}
+const int ITERATIONS = 4;
+bool[,] img = new bool[input[2].Length + 2 * ITERATIONS, input.Length - 2 + 2 * ITERATIONS];
+for (int y = 2; y < input.Length; y++)
+{
+	for (int x = 0; x < input[y].Length; x++)
+	{
+		img[y - 2 + ITERATIONS, x + ITERATIONS] = input[y][x] == '#';
+	}
+}
+for (int x = 0; x < img.GetUpperBound(0) + 1; x++)
+{
+	for (int y = 0; y < img.GetUpperBound(1) + 1; y++)
+	{
+		char vis = img[x, y] ? '#' : '.';
+		Console.Write(vis);
+	}
+	Console.Write('\n');
+}
+Console.WriteLine("Image parsed");
+int ones = 0;
+foreach (var item in img)
+{
+	if (item)
+	{
+		ones++;
+	}
+}
+Console.WriteLine($"{ones} pixels lit up");
+// DO STUFF
+for (int i = 0; i < ITERATIONS; i++)
+{
+	bool[,] newImg;
+	if (img.Clone() is bool[,] cloned)
+	{
+		newImg = cloned;
+	}
+	else
+	{
+		throw new Exception("WHAT?");
+	}
+	for (int x = 0; x < img.GetUpperBound(0) + 1; x++)
+	{
+		for (int y = 0; y < img.GetUpperBound(1) + 1; y++)
+		{
+			int newPixelIndex = 0;
+			int pow = 0;
+			for (int xOff = 1; xOff >= -1; xOff--)
+			{
+				for (int yOff = 1; yOff >= -1; yOff--)
+				{
+					if (x + xOff >= 0 && y + yOff >= 0
+						&& x + xOff <= img.GetUpperBound(0)
+						&& y + yOff <= img.GetUpperBound(1))
+					{ // in bounds
+						if (img[x + xOff, y + yOff])
+						{
+							newPixelIndex += (int)Math.Pow(2, pow); 
+						}
+					}
+					else
+					{ // out of bounds
+						if (i % 2 != 0)
+						{
+							newPixelIndex += (int)Math.Pow(2, pow);
+						}
+					}
+					pow++;
+				}
+			}
+			newImg[x, y] = algo[newPixelIndex];
+		}
+	}
+	img = newImg;
+	for (int x = 0; x < img.GetUpperBound(0) + 1; x++)
+	{
+		for (int y = 0; y < img.GetUpperBound(1) + 1; y++)
+		{
+			char vis = img[x, y] ? '#' : '.';
+			Console.Write(vis);
+		}
+		Console.Write('\n');
+	}
+	Console.WriteLine($"Image enhanced {i+1} times");
+	ones = 0;
+	foreach (var item in img)
+	{
+		if (item)
+		{
+			ones++;
+		}
+	}
+	Console.WriteLine($"{ones} pixels lit up");
+	if (i == 1)
+	{
+		break;
+	}
+}
